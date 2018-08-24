19e002153bda4fce21ad6867fcd28a196ee45296
Phyrone <phyrone@phyrone.de>
2018-08-24 23:16:53 +0200

    add Plugin Base

---
+++ LibyLoader-Plugin/src/main/java/de/phyrone/libloader/bukkit/BukkitPlugin.java
@@ -0,0 +1,6 @@
+package de.phyrone.libloader.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitPlugin extends JavaPlugin {
+}
---
+++ LibyLoader-Plugin/src/main/java/de/phyrone/libloader/bungee/BungeePlugin.java
@@ -0,0 +1,6 @@
+package de.phyrone.libloader.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeePlugin extends Plugin {
+}
